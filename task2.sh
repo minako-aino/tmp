d4d4450eb0e6f0334c02c513a24f930a4b8ee361
minako-aino <79149650+minako-aino@users.noreply.github.com>
2022-05-19 20:24:20 +0200

    Update task2.sh

--- task2.sh
+++ task2.sh
@@ -25,3 +25,7 @@ sudo iptables -A PREROUTING -t nat -p tcp -m multiport --dports 80 -j REDIRECT -
 # the server cannot send emails (SMTP)
 sudo iptables -A OUTPUT -p tcp --dport 25 -j REJECT
 
+# drop all - basic
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT DROP
